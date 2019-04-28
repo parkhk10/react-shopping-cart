1ece7bfecff7710a092be57d646cc5d241bc572b
Keren Park <parkhk10@gmail.com>
2019-04-28 00:05:53 -0500

    add available sizes to each product container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import ProductContainer from './components/ProductContainer'
 
+const mainPageStyle = {
+  textAlign: 'center'
+}
+
+const titleStyle = {
+  marginBottom: '2em',
+  marginTop: '1em'
+}
+
 const App = ({products}) => {
   const skus = Object.keys(products);
   const items = skus.map(
     sku => <ProductContainer 
               productImg={products[sku].sku} 
               productTitle={products[sku].title} 
+              productDesc={products[sku].description}
               productPrice={products[sku].price} 
+              productSizes={products[sku].availableSizes}
             />
     );
 
   return(
-    <div>
-      <h1>Sick Tees Co.</h1>
+    <div style={mainPageStyle}>
+      <h1 style={titleStyle}>Sick Tees Co.</h1>
       <div class="ui grid">{items}</div>
     </div>
     // <ProductContainer productTitle={items} />
--- src/components/ProductContainer.js
+++ src/components/ProductContainer.js
@@ -7,15 +7,23 @@ const container = {
 class ProductContainer extends React.Component {
     constructor(props) {
         super(props)
-      }
+      };
+
+    createSizeButtons = () => {
+        return(
+            this.props.productSizes.map(size => <button class="ui inverted violet button">{size}</button>)
+        )
+    }
 
     render() {
         return(
             <div style={container}>
                 <img src={"./data/products/" + this.props.productImg + "_1.jpg"} />
                 <h3>{this.props.productTitle}</h3>
+                <p>{this.props.productDesc}</p>
                 <p>${this.props.productPrice}</p>
-                <button class="ui secondary button">Add to Cart</button>
+                <div>{this.createSizeButtons()}</div>
+                {/* <button class="ui inverted teal button">Add to Cart</button> */}
                 <br />
                 <br />
                 <br />
